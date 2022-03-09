a2e05193baf571cc5120ffdc2888d5e330e60e09
Derrick Maurais <DerrickMaurais1@gmail.com>
2022-03-09 15:59:40 -0400

    add a little bit of javas ript

---
+++ sketch.js
@@ -0,0 +1,7 @@
+let grid = document.getElementById("#drawpad");
+let choice = document.getElementsByClassName(".sizeslider");
+let color = document.getElementsByClassName(".colorpicker");
+let reset = document.getElementsByClassName(".reset");
+let rbm = document.getElementsByClassName(".rbm");
+
+
