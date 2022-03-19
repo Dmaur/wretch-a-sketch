9ce5e2b2ecef35ad1a9766007ebe8e947a1617c2
Derrick Maurais <derrickmaurais1@gmail.com>
2022-03-19 16:09:06 -0300

    started bfore i was ready. got ahead of myself. nuked and starting over.

--- sketch.js
+++ sketch.js
@@ -3,5 +3,3 @@ let choice = document.getElementsByClassName(".sizeslider");
 let color = document.getElementsByClassName(".colorpicker");
 let reset = document.getElementsByClassName(".reset");
 let rbm = document.getElementsByClassName(".rbm");
-
-
