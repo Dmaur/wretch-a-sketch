5c0900c6ffa9fe920784f84492ef81d931475c44
Derrick <derrickmaurais1@gmail.com>
2022-04-07 19:20:15 -0300

    got the grid working. how divs in the grid changing on mouse over

--- sketch.js
+++ sketch.js
@@ -1,5 +1,21 @@
-let grid = document.getElementById("#drawpad");
-let choice = document.getElementsByClassName(".sizeslider");
-let color = document.getElementsByClassName(".colorpicker");
-let reset = document.getElementsByClassName(".reset");
-let rbm = document.getElementsByClassName(".rbm");
+let grid = document.getElementById("#container");
+let slider = document.getElementsByClassName("slider");
+
+
+
+function makeRows(rows, cols) {
+  container.style.setProperty("--grid-rows", rows);
+  container.style.setProperty("--grid-cols", cols);
+  for (c = 0; c < rows * cols; c++) {
+    let cell = document.createElement("div");
+    container.appendChild(cell).className = "grid-item";
+  }
+}
+makeRows(100,100)
+let boxes = document.getElementsByClassName("grid-item");
+
+for (let box of boxes){
+  box.addEventListener("mouseover", function(event) {
+  event.target.style.background = "black"
+  });
+}
